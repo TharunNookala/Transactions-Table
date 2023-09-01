49da536842961869760f0bcb0c8f205fda5fb4f5
TharunNookala <iamtharun7799@gmail.com>
2023-09-01 19:29:45 +0530

    fix: lowercase conversion bug

--- src/components/UserNameCard/UserNameCard.jsx
+++ src/components/UserNameCard/UserNameCard.jsx
@@ -2,7 +2,7 @@
 const UserNameCard = ({ person }) => {
     return (
         <div className='flex items-center justify-evenly'>
-            <img src={`https://flagcdn.com/48x36/${person.country.toLowerCase()}.png`} alt="..." width={25} />
+            <img src={`https://flagcdn.com/48x36/${person?.country?.toLowerCase()}.png`} alt="..." width={25} />
             <span> {person.name} </span>
         </div>
     )
